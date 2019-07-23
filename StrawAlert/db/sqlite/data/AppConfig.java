fd1cfb560ae430d030c5cb7acc8d98b9dde55b92
domi20u <d20.urbaniak@gmail.com>
2019-07-23 19:34:56 +0200

    Add files via upload

---
+++ StrawAlert/db/sqlite/data/AppConfig.java
@@ -0,0 +1,15 @@
+package de.tum.mw.ftm.praktikum.androidapp_strohhalm.db.sqlite.data;
+
+/**
+ * Created by felix on 18.01.2017.
+ */
+
+public class AppConfig {
+    public static final String URL_Login = "http://pr-android.ftm.mw.tum.de/android/straw/login.php";
+    public static final String URL_Register = "http://pr-android.ftm.mw.tum.de/android/straw/register.php";
+    public static final String URL_InsertPatient = "http://pr-android.ftm.mw.tum.de/android/straw/insertpatient.php";
+    public static final String URL_InsertDrinkEvent = "http://pr-android.ftm.mw.tum.de/android/straw/insertdrinkevent.php";
+    public static final String URL_getPatients = "http://pr-android.ftm.mw.tum.de/android/straw/getPatients.php";
+    public static final String URL_getDrinkevents = "http://pr-android.ftm.mw.tum.de/android/straw/getDrinkevents.php";
+
+}
