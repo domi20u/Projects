bdc6a5e3b7be9fec5857351e4d7343185318959d
domi20u <d20.urbaniak@gmail.com>
2021-04-25 15:08:03 +0200

    Delete DMPs & PI2 directory

--- DMPs & PI2/dE_spring3D.m
+++
@@ -1,9 +0,0 @@
-%% Differential equation for a spring-damper-system
-
-function [yd_, zd_] = dE_spring3D(xs_spring,tau,spring_const,damping,x_end,mass)
-y_ = xs_spring(1,1:3);
-z_ = xs_spring(1,4:6);
-yd_ = z_/tau;
-zd_ = (-spring_const*(y_-x_end) - damping*z_)/(mass*tau);
-end
-
